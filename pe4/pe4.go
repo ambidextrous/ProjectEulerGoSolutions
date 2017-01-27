bc8492d0ac08cc1040a2878edbcf174483000bcc
Aidan <butleraidan@gmail.com>
2017-01-27 00:06:00 +0000

    Problem 4. Concurrent and non Concurrent solutions.

---
+++ pe4/pe4.go
@@ -0,0 +1,66 @@
+//Project Euler: Problem 4
+
+//Largest palindrome product
+
+//A palindromic number reads the same both ways. The largest
+//palindrome made from the product of two 2-digit numbers is
+//9009 = 91 × 99.
+
+//Find the largest palindrome made from the product of two
+//3-digit numbers.
+
+package main
+
+import "fmt"
+import "time"
+
+func isPalindrome(s string) bool {
+	if len(s) <= 1 {
+		return true
+	}
+	if s[0] != s[len(s)-1] {
+		return false
+	}
+	s = s[:len(s)-1]
+	s = s[1:]
+	return isPalindrome(s)
+}
+
+func main() {
+
+	start := time.Now()
+
+	highestPal := 0
+
+	limit := 1000
+
+	for i := limit - 1; i > 0; i-- {
+
+		for j := i - 1; j > 0; j-- {
+
+			candidate := i * j
+
+			s := fmt.Sprintf("%d", candidate)
+
+			if isPalindrome(s) {
+
+				if candidate > highestPal {
+
+					highestPal = candidate
+				}
+			}
+		}
+	}
+
+	elapsed := time.Since(start)
+
+	//num := 1000 * 999
+	//s := fmt.Sprintf("%d", num)
+	//output := isPalindrome(s)
+	//fmt.Println(output)
+	//output = isPalindrome("101")
+	//fmt.Println(output)
+	//output = isPalindrome("9")
+	fmt.Println(highestPal)
+	fmt.Println("Programme execution time %s", elapsed)
+}
---
+++ pe4/pe4Concurrent.go
@@ -0,0 +1,77 @@
+//Project Euler: Problem 4
+
+//Largest palindrome product
+
+//A palindromic number reads the same both ways. The largest
+//palindrome made from the product of two 2-digit numbers is
+//9009 = 91 × 99.
+
+//Find the largest palindrome made from the product of two
+//3-digit numbers.
+
+// Note:
+// Brute force solution launching 1000 goroutines outperforms
+// the same algorithms without goroutines by a factor of
+// five: 14 ms vs. 72 ms.
+
+package main
+
+import "fmt"
+import "time"
+
+func isPalindrome(s string) bool {
+	if len(s) <= 1 {
+		return true
+	}
+	if s[0] != s[len(s)-1] {
+		return false
+	}
+	s = s[:len(s)-1]
+	s = s[1:]
+	return isPalindrome(s)
+}
+
+func getHighestPal(i int, c chan int) {
+
+	for j := i - 1; j > 0; j-- {
+
+		candidate := i * j
+
+		s := fmt.Sprintf("%d", candidate)
+
+		if isPalindrome(s) {
+
+			c <- candidate
+		}
+	}
+}
+
+func main() {
+
+	start := time.Now()
+
+	highestPal := 0
+
+	limit := 1000
+
+	c := make(chan int)
+
+	for i := limit - 1; i > 0; i-- {
+
+		go getHighestPal(i, c)
+	}
+
+	for i := 0; i < limit; i++ {
+
+		candidate := <-c
+
+		if candidate > highestPal {
+
+			highestPal = candidate
+		}
+	}
+
+	elapsed := time.Since(start)
+	fmt.Println(highestPal)
+	fmt.Println("Programme execution time %s", elapsed)
+}
